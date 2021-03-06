107246270b1387d71d48ae3cdde8ea6973164103
Zack Turman <zkturman@gmail.com>
2021-03-06 23:38:46 +0000

    Reorganized and added checkpoint UI for player
    Reorganized and added checkpoint UI for player

---
+++ Assets/cCheckpointManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cCheckpointManager : MonoBehaviour
+{
+    public cCheckpointBeacon[] lanterns;
+    public int numberLit;
+    public int totalLanterns;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lanterns = FindObjectsOfType<cCheckpointBeacon>();
+        if (lanterns != null)
+        {
+            totalLanterns = lanterns.Length;
+        }
+        numberLit = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int count = 0;
+        foreach (cCheckpointBeacon x in lanterns)
+        {
+            if (x.lightOn)
+            {
+                count++;
+            }
+        }
+        numberLit = count;
+    }
+}
---
+++ Assets/cPlayerUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cPlayerUI : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI checkpointGUI;
+    public cCheckpointManager checkpoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointGUI = GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        if (checkpointGUI == null)
+        {
+            Debug.LogError("Not able to get text mesh for player GUI.");
+        }
+        checkpoints = FindObjectOfType<cCheckpointManager>();
+        if (checkpoints == null)
+        {
+            Debug.LogError("Not able to get checkpoint object from cCheckpointManager");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkpointGUI.text = checkpoints.numberLit + " of " + checkpoints.totalLanterns;
+    }
+}
